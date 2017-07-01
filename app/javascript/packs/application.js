38d088b5c50adee4e9731aa98b3ad1af9dc8cf55
Petr Gazarov <petrgazarov@gmail.com>
2017-07-01 16:51:36 -0400

    Initial commit

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,22 @@
+// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
+// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
+// of the page.
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+
+class Hello extends React.Component {
+  render() {
+    return(
+      <div>Bonanza!</div>
+    );
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <Hello />,
+    document.body.appendChild(document.createElement('div')),
+  )
+})
