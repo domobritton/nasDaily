0b4f40d0b41313e37007c202b0bc5038a64b585b
Petr Gazarov <petrgazarov@gmail.com>
2017-07-09 19:17:39 -0400

    Refactor controller and fetch higher res pictures

---
+++ app/commands/videos/fetch.rb
@@ -0,0 +1,102 @@
+require 'koala'
+
+class Videos::Fetch
+  def self.call
+    new.call
+  end
+
+  def graph
+    return @graph if @graph
+
+    oauth = Koala::Facebook::OAuth.new(ENV.fetch('FB_APP_ID'), ENV.fetch('FB_APP_SECRET'))
+    access_token = oauth.get_app_access_token
+
+    @graph = Koala::Facebook::API.new(access_token)
+  end
+
+  def call
+    raw_videos = graph.get_object(
+      'nasdaily/videos?fields=content_tags,description,title,picture&limit=2000'
+    )
+
+    # preload all videos and content_tags
+    Video.includes(:content_tags).all.to_a
+
+    raw_videos.each do |v|
+      video_model = Video.find_by(facebook_id: v['id'])
+
+      # add only new videos, cache already fetched ones
+      next if video_model
+
+      video_model = create_video_if_horizontal!(v)
+      # skip vertical videos and videos without a thumbnail
+      next unless video_model
+
+      if v['content_tags']
+        v['content_tags'].each do |t|
+          # rescue because facebook's content_tag ids don't work sometimes
+          tag = graph.get_object("#{t}?fields=name") rescue nil
+          next unless tag
+
+          tag_model = find_or_create_tag_model!(tag)
+          find_or_create_videos_content_tag_model!(tag_model, video_model)
+        end
+      end
+    end
+
+    Video.includes(:content_tags).all.order(:created_at)
+  end
+
+  private
+
+  def create_video_if_horizontal!(raw_video)
+    format = graph.get_object("#{raw_video['id']}?fields=format")['format']
+
+    # skip videos without thumbnails
+    return nil unless format.count > 1
+    # skip vertical videos
+    return nil unless format.first['width'] > format.first['height']
+
+    full_picture = format
+      .select { |f| f['width'] == 480 }
+      .first['picture']
+
+    Video.create!(
+      facebook_id: raw_video['id'],
+      title: raw_video['title'],
+      description: raw_video['description'],
+      picture: raw_video['picture'],
+      full_picture: full_picture,
+    )
+  end
+
+  def find_or_create_tag_model!(raw_tag)
+    model = ContentTag.find_by(facebook_id: raw_tag['id'])
+
+    if !model
+      model = ContentTag.create!(
+        facebook_id: raw_tag['id'],
+        name: raw_tag['name']
+      )
+    end
+
+    model
+  end
+
+
+  def find_or_create_videos_content_tag_model!(tag, video)
+    model = VideosContentTag.find_by(
+      content_tag: tag,
+      video: video
+    )
+
+    if (!model)
+      model = VideosContentTag.create!(
+        content_tag: tag,
+        video: video
+      )
+    end
+
+    model
+  end
+end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,69 +1,16 @@
-require 'koala'
-
 class HomeController < ApplicationController
   def index
   end
 
   def cached_videos
-    render json: Rails.cache.read('all_videos')
+    videos = Rails.cache.read('all_videos') || []
+
+    render json: videos
   end
 
   def videos
-    oauth = Koala::Facebook::OAuth.new(ENV.fetch('FB_APP_ID'), ENV.fetch('FB_APP_SECRET'))
-    access_token = oauth.get_app_access_token
-    graph = Koala::Facebook::API.new(access_token)
-
-    videos = graph.get_object(
-      'nasdaily/videos?fields=content_tags,description,title,picture&limit=2000'
-    )
-
-    Video.includes(:content_tags).all.to_a
-
-    videos.map! do |v|
-      video_model = Video.find_by(facebook_id: v['id'])
-
-      next if video_model
-
-      video_model = Video.create!(
-        facebook_id: v['id'],
-        title: v['title'],
-        description: v['description'],
-        picture: v['picture'],
-        full_picture: v['picture'],
-      )
-
-      if v['content_tags']
-        v['content_tags'].each do |t|
-          tag = graph.get_object("#{t}?fields=name") rescue nil
-
-          next unless tag
-
-          tag_model = ContentTag.find_by(facebook_id: tag['id'])
-
-          if !tag_model
-            tag_model = ContentTag.create!(
-              facebook_id: tag['id'],
-              name: tag['name']
-            )
-          end
-
-          videos_content_tag_model = VideosContentTag.find_by(
-            content_tag: tag_model,
-            video: video_model
-          )
-
-          if (!videos_content_tag_model)
-            videos_content_tag_model = VideosContentTag.create!(
-              content_tag: tag_model,
-              video: video_model
-            )
-          end
-        end
-      end
-    end
-
-    @videos = Video.includes(:content_tags).all
-    json_videos = videos_to_json(@videos)
+    videos = Videos::Fetch.call
+    json_videos = videos_to_json(videos)
     Rails.cache.write('all_videos', json_videos)
 
     render json: json_videos
