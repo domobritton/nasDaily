0bebed9ce565ae0946cb57a3dafc8301bd31bd2b
Petr Gazarov <petrgazarov@gmail.com>
2017-07-22 17:37:08 -0400

    Remove user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,4 @@
 class User < ApplicationRecord
   validates :email, presence: true
   validates_format_of :email, with: /.+@.+\..+/
-  validates :early_interest_in_app,
-            :early_interest_in_tshirt,
-            presence: true
 end
