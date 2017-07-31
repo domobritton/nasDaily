3c2e7ad41eb651b47c3462b9b4fb71931bb743f8
Petr Gazarov <petrgazarov@gmail.com>
2017-07-30 21:37:56 -0400

    app tab style and deletes code

--- app/javascript/packs/components/AppTab/index.js
+++ app/javascript/packs/components/AppTab/index.js
@@ -5,10 +5,10 @@ export default class AppTab extends React.Component {
   render() {
     return (
       <div className='nd-app'>
-        <h1>NASDAILY APP.</h1>
-        <p>The best way to create and discover videos. Coming soon.</p>
+        <h1>NASDAILY <span className='yellow-color'>APP.</span></h1>
+        <p>The best way to create and discover videos. <span className='yellow-color'>Coming soon.</span></p>
         <EmailForm submitOptions={{ early_interest_in_app: true }}/>
-        <p>That's 1 minute, see you tomorrow.</p>
+        <p>That's 1 minute, <span className='yellow-color'>see you tomorrow.</span></p>
       </div>
     );
   }
