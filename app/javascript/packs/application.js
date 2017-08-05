50de01fd2134aaccc560b268d8f5951a1f41dea9
Petr Gazarov <petrgazarov@gmail.com>
2017-08-05 13:05:57 -0400

    Make jquery global for debugging

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { CloudinaryContext } from 'cloudinary-react';
 import App from './components/App';
+import $ from 'jquery';
 
 document.addEventListener('DOMContentLoaded', () => {
+  window.$ = $; // make jquery global for debugging
+
   const WrappedApp = () => (
     <BrowserRouter>
       <CloudinaryContext cloudName="nasdaily">
