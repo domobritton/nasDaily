8f680956589279537edc71f516e492f5b4028944
Petr Gazarov <petrgazarov@gmail.com>
2017-08-17 23:53:23 -0400

    Implement new form

---
+++ app/javascript/packs/components/ShopTab/constants.js
@@ -0,0 +1,12 @@
+export const genderOptions = [
+  ['', 'Choose'],
+  ["male", "Male"],
+  ["female", "Female"],
+  ["other", "Other"]
+]
+
+export const countryOptions = [
+  ['', 'Choose'],
+  ['usa', 'United States'],
+  ['israel', 'Israel']
+]
--- app/javascript/packs/components/ShopTab/index.js
+++ app/javascript/packs/components/ShopTab/index.js
@@ -1,16 +1,93 @@
 import React from 'react';
 import EmailForm from '../EmailForm';
+import { genderOptions, countryOptions } from './constants';
 
 export default class ShopTab extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      age: '',
+      gender: '',
+      country: ''
+    }
+
+    this.setAge = this.setAge.bind(this);
+  }
+
+  setAge(e) {
+    const { value } = e.target;
+
+    if (isFinite(value) && Number(value) >= 0 && Number(value) < 126) {
+      this.setState({ age: value });
+    }
+  }
+
   render() {
+    const { age, gender, country } = this.state;
+
     return (
       <div className='nd-shop'>
-        <div>
-          <h1>The Life Shirt</h1>
-          <br/>
-          <p className='yellow-color'>Coming your way soon.</p>
-          <EmailForm submitOptions={{ early_interest_in_tshirt: true }}/>
-        </div>
+        <form className='form'>
+          <label>Your <span className='white-color'>Age</span></label>
+          <div className='input-group'>
+            <div className='input-wrapper'>
+              <input
+                placeholder='Enter your age'
+                value={age}
+                onChange={this.setAge}
+              />
+            </div>
+          </div>
+          <label>Your <span className='white-color'>Gender</span></label>
+          <div className='input-group'>
+            <div className='select-wrapper'>
+              <select
+                value={ gender }
+                onChange={({target: {value}}) => { this.setState({ gender: value })}}
+              >
+                {
+                  genderOptions.map((o, idx) => (
+                    <option
+                      key={idx}
+                      value={o[0]}
+                      disabled={ idx === 0 ? true : false }
+                    >
+                      {o[1]}
+                    </option>
+                  ))
+                }
+              </select>
+            </div>
+          </div>
+          <label>Your <span className='white-color'>Country</span></label>
+          <div className='input-group'>
+            <div className='select-wrapper'>
+              <select
+                value={ country }
+                onChange={({target: {value}}) => { this.setState({ country: value })}}
+              >
+                {
+                  countryOptions.map((o, idx) => (
+                    <option
+                      key={idx}
+                      value={o[0]}
+                      disabled={ idx === 0 ? true : false }
+                    >
+                      {o[1]}
+                    </option>
+                  ))
+                }
+              </select>
+            </div>
+          </div>
+          <button
+            type='submit'
+            className='submit-button'
+          >
+            Calculate
+          </button>
+        </form>
       </div>
     );
   }
