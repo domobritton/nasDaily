e1ecc0185784749b0162e0b9bc2baabd1428ac74
Petr Gazarov <petrgazarov@gmail.com>
2017-08-26 22:10:56 -0400

    Different layout on mobile for shop form

--- app/javascript/packs/components/ShopTab/index.js
+++ app/javascript/packs/components/ShopTab/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import Slider from 'react-slick';
 import FacebookPlayer from 'react-facebook-player';
 import ReactModal from 'react-modal';
+import {Collapse} from 'react-collapse';
+import $ from 'jquery';
 import { sliderSettings } from './constants';
 import { modalStyles } from '../VideosTab/constants';
 import facebookAppId from '../../util/facebookAppId';
 import ShopTabForm from './Form';
+import { isMobile } from '../../util/viewportSize';
 
 export default class ShopTab extends React.Component {
   constructor() {
@@ -13,11 +16,16 @@ export default class ShopTab extends React.Component {
 
     this.state = {
       openedModal: false,
+      showFormOnMobile: false
     }
+
+    this.showForm = this.showForm.bind(this);
   }
 
   get videoModal() {
-    const { openedModal } = this.state;
+    const {
+      openedModal,
+    } = this.state;
 
     return (
       <ReactModal
@@ -37,52 +45,80 @@ export default class ShopTab extends React.Component {
     );
   }
 
+  showForm() {
+    if (isMobile()) {
+      this.setState({showFormOnMobile: true});
+
+      setTimeout(() =>
+        $('html, body').animate({ scrollTop: $('form').position().top }, 600, 'swing')
+      );
+    } else {
+      this.sliderElement.slickNext()
+    }
+  }
+
+  get mainSection() {
+    return (
+      <div className='landing'>
+        <div className='landing--left'>
+          <div className='landing--left-background'/>
+          <div className='landing--left-content'>
+            <h2><span className='yellow-color'>The</span> Story</h2>
+            <p>Watch the video behind the story of Nas Daily T-shirts</p>
+            <a
+              className='shop-cta'
+              onClick={() => this.setState({openedModal: true})}
+            >
+              Watch now
+            </a>
+          </div>
+        </div>
+        <div className='landing--right'>
+          <div className='landing--right-background'/>
+          <div className='landing--right-content'>
+            <h2><span className='yellow-color'>The</span> Tshirt</h2>
+            <p>Custom made for you</p>
+            <a
+              className='shop-cta'
+              onClick={this.showForm}
+            >
+              Buy now
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   get slider() {
+    if (isMobile()) { return null }
+
     return (
       <Slider
         { ...sliderSettings }
         ref={(ref) => this.sliderElement = ref}
       >
-        <div className='landing'>
-          <div className='landing--left'>
-            <div className='landing--left-background'/>
-            <div className='landing--left-content'>
-              <h2><span className='yellow-color'>The</span> Story</h2>
-              <p>Watch the video behind the story of Nas Daily T-shirts</p>
-              <a
-                className='shop-cta'
-                onClick={() => this.setState({openedModal: true})}
-              >
-                Watch now
-              </a>
-            </div>
-          </div>
-          <div className='landing--right'>
-            <div className='landing--right-background'/>
-            <div className='landing--right-content'>
-              <h2><span className='yellow-color'>The</span> Tshirt</h2>
-              <p>Custom made for you</p>
-              <a
-                className='shop-cta'
-                onClick={() => this.sliderElement.slickNext()}
-              >
-                Buy now
-              </a>
-            </div>
-          </div>
-        </div>
+        { this.mainSection }
         <div className='form-section'>
-          <ShopTabForm />
+          { isMobile() ? null : <ShopTabForm /> }
         </div>
       </Slider>
     );
   }
 
   render() {
+    const { showFormOnMobile } = this.state;
+
     return (
       <div className='nd-shop'>
         { this.slider }
+        { isMobile() && this.mainSection }
         { this.videoModal }
+        <Collapse
+          isOpened={isMobile() && showFormOnMobile}
+        >
+          <ShopTabForm />
+        </Collapse>
       </div>
     );
   }
